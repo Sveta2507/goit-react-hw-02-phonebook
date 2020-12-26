69d8b6e6c1f118e9e518f7565007b03608c2c06b
Sveta2507 <dnsdns.dn@googlemail.com>
2020-12-26 13:09:45 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import React, { Component } from "react";
 import "./App.css";
-import Form from "./components/Form";
+import Form from "./components/Form.js"
+import ContactList from './components/ContactList.js'
 
 class App extends Component {
   state = {
     contacts: [],
     name: "",
   };
+  addContact = (contact) => {
+    this.setState({
+      contacts: [...this.state.contacts, contact],
+    });
+  };
   render() {
     return (
       <>
-        <h1>Phonebook</h1>
-        <Form />
+        <h1>PhoneBook</h1>
+        <Form addContact={this.addContact}/>
+        <ContactList contacts={this.state.contacts} />
       </>
     );
   }
